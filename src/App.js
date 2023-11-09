2abe310bcc9d1e468058d101f503d4c5abcce3a7
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-11-09 11:56:00 +0500

    Create react project, add linters and install all required dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" />
+    </Routes>
+  </Router>
+);
+
+export default App;
