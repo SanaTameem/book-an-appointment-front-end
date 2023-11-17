3d9c395ca25f5141003c4c9480184fe8f96b3095
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-11-17 15:52:02 +0430

    Fix navbar styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+// import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import RequireAuth from './components/RequireAuth';
