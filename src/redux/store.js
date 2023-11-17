5359e4def0a956a75c5d32c5b3c2062a52a5dcc1
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-17 15:51:19 +0900

    implemnt the store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import carsReducer from "./features/carsSlice";
 
 const store = configureStore({
   reducer: {
+    cars: carsReducer,
   },
 });
 
