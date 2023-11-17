d6b3407380682e4fd9153614d090b65ff9951094
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-11-17 15:37:15 +0430

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import RequireAuth from './components/RequireAuth';
-import AddCarsForm from './app/cars/AddCarsForm';
+import AddCarsForm from './components/cars/AddCarsForm';
+// import Navbar from './components/Navbar';
+import MainPage from './components/cars/MainPage';
+import DeleteCar from './components/cars/DeleteCar';
+import Reservations from './components/reservations/Reservations';
+import CarDetails from './components/cars/CarDetails';
 
 const App = () => (
-  <div className="container-fluid main-height ">
-    <main className="col">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route element={<RequireAuth />}>
-          <Route exact path="/" element={<AddCarsForm />} />
-        </Route>
-      </Routes>
-    </main>
-  </div>
+  // <div className="container-fluid main-height ">
+  // <main className="col">
+  <>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<RequireAuth />}>
+        <Route exact path="/" element={<AddCarsForm />} />
+        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/details/:id" element={<CarDetails />} />
+        <Route path="/cars/delete" element={<DeleteCar />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Route>
+    </Routes>
+  </>
+  // </main>
+  // </div>
 );
 
 export default App;
