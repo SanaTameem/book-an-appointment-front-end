1bc7833624a4388be6b83a55eab680055febd071
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-11-17 15:38:04 +0430

    Add src/components/cars/ folder

---
+++ src/components/cars/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
+function MainPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="main-page-container">MainPage</div>
+    </>
+  );
+}
+
+export default MainPage;
